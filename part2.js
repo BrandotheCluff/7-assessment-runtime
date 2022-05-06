0a610b258b4eb6360be38ecd824ba60331696fe2
BrandotheCluff <brandon.cluff2@gmail.com>
2022-05-06 16:14:05 -0600

    finished

--- part2.js
+++ part2.js
@@ -13,6 +13,10 @@
 // }    
 // console.log(equalZero([1, 2, 3, -2]))
 
+// O(n^2)
+
+
+
 // const uniqChar = str => {
 //     let wordArr = str.split('')
 //     let unique = true
@@ -29,7 +33,49 @@
 // }
 // console.log(uniqChar('Monday'))
 
-const pangram = str => {
-    let alphArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    let wordArr = str.split('')
-}
+// O(n^2)
+
+
+
+// const pangram = str => {
+//     // let alphArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+//     // let wordArr = str.split('')
+//     str = str.toLowerCase().split('').filter((e) => e >= "a" && e <= "z").length > 25;
+
+//     let map = {}
+
+//     for (let i = 0; i < str.length; i++){
+//         let letter = str[i]
+//         if(map[letter]){
+//             map[letter] += 1
+//         }else {
+//             map[letter] = 1
+//         }
+//     }
+//     let count = 0
+
+//     for(let key in map){
+//         count++
+//     }
+//     if(count >= 26){
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// O(n)
+
+
+
+// const findLongest = arr => {
+//     let wordLength = 0
+//     arr.forEach(word => {
+//         if(word.length > result){
+//             result = word.length
+//         }
+//     })
+//     return result 
+// }
+
+// O(n)
